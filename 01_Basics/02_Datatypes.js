e6e35bd0f9abbd6d09d2b1b41e524e9c76068f2f
DeepakYadav-17 <oy32140@gmail.com>
2025-08-08 16:42:57 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // Treat all js code as newer version.
+/* alert(3+3);  We are using node.js engine, not brower engine*/
+
+let name = "Maru";
+let age = 23;
+let isLogedIn = true;
+
+let state = null;
+let city;
+
+/*
+
+Primitive datatype
+
+number => 2 to power of 53
+bigInt
+string => " ";  ' ';
+boolean => true/false
+null => standaloan value empty value
+undefine => It is undefined. In which we do not assign a value
+symbol => unique used in react mostly.
+
+Non primitive datatype
+
+object
+
+*/
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof city); //undefined
+console.table(typeof [name, age, isLogedIn]); //object
