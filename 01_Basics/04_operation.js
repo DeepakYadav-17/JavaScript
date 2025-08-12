ca6515dd8407633f1b8239c20592c76b60ab8400
DeepakYadav-17 <oy32140@gmail.com>
2025-08-12 05:53:16 +0000

    operation done

---
+++ 01_Basics/04_operation.js
@@ -0,0 +1,41 @@
+let value = 3;
+
+let negValue = -value;
+console.log(negValue);
+
+/*
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2**3);  2 ka power 3
+console.log(2/3);
+console.log(2%3); remainder
+
+*/
+
+let str1 = "Maru";
+let str2 = " Deepak";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + " 2");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + "2" + 2);
+
+console.log(true);                 //true
+console.log(+true);               //1
+// console.log(true+);            error
+// console.log(true++);           error
+console.log(+"");                //0
+
+let gameCounter = 100
+gameCounter++;                   //post increement    prefix / postfix difernece mdn docx        
+++gameCounter;                   //pre increment
+console.log(gameCounter);       //102
+
+
+
