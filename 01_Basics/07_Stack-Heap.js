4504d4c618064774ceb9941c43e1296650d0cd64
DeepakYadav-17 <oy32140@gmail.com>
2025-08-15 11:45:30 +0000

    stack-heap done

--- 01_Basics/07_Stack-Heap.js
+++ 01_Basics/07_Stack-Heap.js
@@ -1,12 +1,14 @@
 // Stack (primitive datatype).    Heap (reference - non primitive datatype).
 // jab bhi stack memory use hoti hai matlab jab bhi variable declare kiya to uska ek copy milta hai.
 //jab bhi ek memory heap ke ander define hoti hai object ya array to yaha se milta 
-// hai refernce jo bhi change karenge original value mai change hoga.
+// hai refernce jo bhi change karenge wo original value mai change hoga.
 
 let myName = "Maru";
 let anotherName = myName
 anotherName = "Deepak";
-
+// jab bhi maine bola ki anothername ke ander jo value hai wo my name se jayegi 
+// to actual me yaha pe value nahi uska ek copy diya jata hai matlab my name ka copy
+//  ab copy m change kiya to original value thodi na change hogi.
 console.log(myName);
 console.log(anotherName);
 
